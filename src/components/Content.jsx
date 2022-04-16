5d23080bc798cb70669383e445a2da4dce979295
12345120 <hehe9343@gmail.com>
2022-04-15 23:31:34 -0400

    basic setup of project
    * node-sass + import scss as module (style)
    * some very basic setup

---
+++ src/components/Content.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from "../styles/Content.module.scss"
+
+function Content() {
+  return <div>
+    <div className={style.text}>
+      asdf
+    </div>
+  </div>;
+}
+
+export default Content;
